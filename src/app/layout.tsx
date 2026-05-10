4d23e61955925239460401d5a82e1b31139753ad
篠崎友寿 <shinozakitomohisa@MacBook-Pro-2.local>
2026-05-11 08:45:29 +0900

    add Google Search Console verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "ニッポン博士への道",
   description: "47都道府県をタイピングして日本の名産・歴史を学ぼう！",
+  verification: {
+    google: "qnAFDsFMEPB4l3oZNT6C4ECgeBpYHjkxwx-FZ8b88QA",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
